6be9b40530a3e8d7602cb853e0da5c0f30381e08
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-10-28 18:41:14 -0300

    Fiz um pequeno CSS na página e adicionei a chave da api e a rota para ser feita a requisição

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,13 @@
 import requests
 
-api = ""
-cidade = ""
-link = f""
+api = "5c1635f4b74726cbfaf1e26689ec5879"
+cidade = "sao paulo"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api}&lang=pt_br"
 
 requisicao = requests.get(link)
 requisicao_dic = requisicao.json()
+name = requisicao_dic['name']
 descricao = requisicao_dic['weather'][0]['description']
 temperatura = requisicao_dic['main']['temp'] - 273.15
-print(descricao, f"{temperatura}°6")
+print(descricao, f"{int(temperatura)}°c em {name}")
+print(requisicao_dic)
