18ab44224e3d9733f62351ebdc05159db6cf285e
JOAO PEDRO ALMEIDA MENINO <00001132521804sp@aluno.educacao.sp.gov.br>
2024-10-29 20:14:19 -0300

    funcionando

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 from flask import Flask, request, render_template 
+import funcoes
+import requests
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def comecando():
-    return render_template("tempo.html")
+    weather_info = None
+
+    if request.method == "POST":    
+        api = "5c1635f4b74726cbfaf1e26689ec5879"
+
+        city = request.form.get("city")
+        link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&lang=pt_br"
+        try:
+            requisicao = requests.get(link)
+            requisicao_dic = requisicao.json()
+            if requisicao.status_code == 200:
+                weather_info = {
+                    "nome": requisicao_dic['name'],
+                    "temp_atual": requisicao_dic['main']['temp'],
+                    "descricao":requisicao_dic['weather'][0]['description'],
+                    "temp_max": requisicao_dic['main']['temp_max'],
+                    "temp_min": requisicao_dic['main']['temp_min'],
+                    "umidade": requisicao_dic['main']['humidity'],
+                    "vento": requisicao_dic['wind']['speed'],
+                    "icone": requisicao_dic['weather'][0]['icon']
+                }
+            else:
+                weather_info = "erro"
+                
+        except:
+            weather_info = "erro"
+        
+    return render_template("tempo.html", weather_info=weather_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- funcoes.py
+++ funcoes.py
@@ -4,17 +4,23 @@ api = "5c1635f4b74726cbfaf1e26689ec5879"
 cidade = "sao paulo"
 link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api}&lang=pt_br"
 
-requisicao = requests.get(link)
-requisicao_dic = requisicao.json()
-name = requisicao_dic['name']
-weather_info = {
- "nome": requisicao_dic['name'],
-  "temp_atual": requisicao_dic['main']['temp'],
-  "descricao":requisicao_dic['weather'][0]['description'],
-  "temp_max": requisicao_dic['main']['temp_max'],
-  "temp_min": requisicao_dic['main']['temp_min'],
-  "umidade": requisicao_dic['main']['humidity'],
-  "vento": requisicao_dic['wind']['speed'],
-  "icone": requisicao_dic['weather'][0]['icon']
-}
-print(weather_info)
+def request(city):
+    api = "5c1635f4b74726cbfaf1e26689ec5879"
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api}&lang=pt_br"
+
+    requisicao = requests.get(link)
+    requisicao_dic = requisicao.json()
+ 
+
+    weather_info = {
+        "nome": requisicao_dic['name'],
+        "temp_atual": requisicao_dic['main']['temp'],
+        "descricao":requisicao_dic['weather'][0]['description'],
+        "temp_max": requisicao_dic['main']['temp_max'],
+        "temp_min": requisicao_dic['main']['temp_min'],
+        "umidade": requisicao_dic['main']['humidity'],
+        "vento": requisicao_dic['wind']['speed'],
+        "icone": requisicao_dic['weather'][0]['icon']
+    }
+    return weather_info
+
